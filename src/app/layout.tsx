59abf67270613b4de93ee94adda5eb6b6ebf4728
junayed-code <junayed.code@gmail.com>
2024-02-24 12:49:37 +0600

    Build app base layout

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,5 +1,9 @@
 import type { Metadata } from "next";
 import { inter, unbounded } from "@/fonts";
+import Sidebar from "@/components/Sidebar";
+import Header from "@/components/Header";
+import ProvidersWrapper from "@/providers/ProvidersWrapper";
+import { RootContainer, PageContainer, Main } from "@/components/composition";
 import "./globals.css";
 
 export const metadata: Metadata = {
@@ -7,14 +11,24 @@ export const metadata: Metadata = {
   description: "",
 };
 
-export default function RootLayout({
+export default async function RootLayout({
   children,
 }: Readonly<{
   children: React.ReactNode;
 }>) {
   return (
     <html lang="en" className={`${inter.variable} ${unbounded.variable}`}>
-      <body className="font-inter">{children}</body>
+      <body className="font-inter overflow-hidden">
+        <RootContainer>
+          <ProvidersWrapper>
+            <Sidebar />
+            <PageContainer>
+              <Header />
+              <Main>{children}</Main>
+            </PageContainer>
+          </ProvidersWrapper>
+        </RootContainer>
+      </body>
     </html>
   );
 }
